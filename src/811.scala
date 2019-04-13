55ea98d75bf1f4d09ae87d76110f220e8c5c0a96
Xiaodong <xd_deng@hotmail.com>
2019-04-13 15:16:16 +0800

    Minor refinement on solution to 811

--- src/811.scala
+++ src/811.scala
@@ -1,22 +1,16 @@
 object Solution {
     def subdomainVisits(cpdomains: Array[String]): List[String] = {
-        
-        var domain_tuples = for (i <- cpdomains.map(x => (x.split(" ")(0), x.split(" ")(1)));
-                               j <- List(-1) ++ (0 until i._2.length).filter(i._2(_) == '.'))
-                           yield (i._1, i._2.drop(j+1))
 
-        domain_tuples = domain_tuples.map(x => (x._2, x._1))
-
+        val domain_tuples = for (i <- cpdomains.map(x => x.split(" "));
+                                 split_domains = i(1).split('.');
+                                 j <- (1 to split_domains.length).map(split_domains.takeRight(_).mkString("."))) 
+          yield (j, i(0))
 
         var result = domain_tuples.groupBy(_._1).mapValues(group => group.map(_._2.toInt).sum)
-        
         // Another way to sum by key
         // var result = for { (key, xs) <- domain_tuples.groupBy(_._1)
-        //       x = xs.map(_._2.toInt).sum
-        //     } yield (key, x)
-
+        //     } yield (key, xs.map(_._2.toInt).sum)
 
         result.map(x => x._2.toString + " " + x._1).toList
-        
     }
 }
